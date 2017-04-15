a0b45aed2f23afa026b75abe92411c71fb72a65a
Will Dicharry <wdicharry@gmail.com>
2017-04-15 09:48:10 -0600

    Remove unsafe use of unique pointer to array.

--- src/FakeCipher.cpp
+++ src/FakeCipher.cpp
@@ -8,39 +8,39 @@
 #include <ctime>
 
 FakeCipher::FakeCipher(std::size_t blockSize) :
-    mBlockSize(blockSize),
-    mDictionary(),
-    mTotalBlocks(0) {
+  mBlockSize(blockSize),
+  mDictionary(),
+  mTotalBlocks(0) {
 }
 
 FakeCipher::~FakeCipher() {
 }
 
 std::size_t FakeCipher::initialize() {
-    std::time_t seed;
-    time(&seed);
-    srand(seed);
-    return mBlockSize;
+  std::time_t seed;
+  time(&seed);
+  srand(seed);
+  return mBlockSize;
 }
 
 CharBlock FakeCipher::encrypt(const CharBlock& block) {
-    mTotalBlocks++;
-    auto storedBlock = mDictionary.find(block);
-    if (storedBlock != mDictionary.end()) {
-        return storedBlock->second;
-    }
+  mTotalBlocks++;
+  auto storedBlock = mDictionary.find(block);
+  if (storedBlock != mDictionary.end()) {
+    return storedBlock->second;
+  }
 
-    std::unique_ptr<char[]> buffer(new char[block.size()]);
-    std::transform(
-        block.bytes().begin(), block.bytes().end(),
-        buffer.get(),
-        [](char _){ return static_cast<char>(rand()); });
-    CharBlock outputBlock(mBlockSize, buffer.get());
-    mDictionary.insert(std::make_pair(block, outputBlock));
-    return outputBlock;
+  std::vector<char> buffer(block.size());
+  std::transform(
+                 block.bytes().begin(), block.bytes().end(),
+                 buffer.begin(),
+                 [](char _){ return static_cast<char>(rand()); });
+  CharBlock outputBlock(mBlockSize, buffer.begin());
+  mDictionary.insert(std::make_pair(block, outputBlock));
+  return outputBlock;
 }
 
 void FakeCipher::outputStats(std::ostream& out) {
-    out << "Processed " << mTotalBlocks
-        << " Blocks (" << mDictionary.size() << " unique)";
+  out << "Processed " << mTotalBlocks
+      << " Blocks (" << mDictionary.size() << " unique)";
 }
